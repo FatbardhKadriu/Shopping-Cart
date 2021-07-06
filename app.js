f33eb63d2e4c24997642e566520f66c2fe0e2890
Fatbardh <fatbardh.kadriu@gmail.com>
2021-07-06 21:04:07 +0200

    clear the cart

--- app.js
+++ app.js
@@ -140,7 +140,7 @@ class UI {
     cartOverlay.classList.remove('transparentBcg');
     cartDOM.classList.remove('showCart');
   }
-  
+
   setupAPP() {
      cart = Storage.getCart();
      this.setCartValues(cart);
@@ -153,9 +153,32 @@ class UI {
      cart.forEach(item => this.addCartItem(item));
   }
 
-  hideCart() {
-    cartOverlay.classList.remove('transparentBcg');
-    cartDOM.classList.remove('showCart');
+  cartLogic() {
+    // clear cart button
+    clearCartBtn.addEventListener('click', () => {
+      this.clearCart();
+    });
+    // cart functionality
+  }
+  clearCart() {
+    let cartItems = cart.map(item => item.id);
+    cartItems.forEach(id => this.removeItem(id));
+    while (cartContent.children.length > 0) {
+      cartContent.removeChild(cartContent.children[0]);
+    }
+    this.hideCart();
+  }
+  removeItem(id) {
+    cart = cart.filter(item => item.id !== id);
+    this.setCartValues(cart);
+    Storage.saveCart(cart);
+    let button = this.getSingleButton(id);
+    button.disabled = false;
+    button.innerHTML = `<i class="fas fa-shopping-cart">add to cart</i>`
+  }
+
+  getSingleButton(id) {
+    return buttonsDOM.find(button => button.dataset.id === id);
   }
 }
 
@@ -188,5 +211,6 @@ document.addEventListener("DOMContentLoaded", () => {
     Storage.saveProducts(products);
   }).then(() => {
     ui.getBagButtons();
+    ui.cartLogic();
   });
 });
