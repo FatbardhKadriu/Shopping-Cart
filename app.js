11d82d1f0872e01bf2a7edfe6a421610c77f1474
Fatbardh <fatbardh.kadriu@gmail.com>
2021-07-06 18:21:28 +0200

    Get products

--- app.js
+++ app.js
@@ -1,25 +1,32 @@
 //variables
 
-const cartBtn = document.querySelector(".cart-btn");
-const closeCartBtn = document.querySelector(".close-cart");
-const clearCartBtn = document.querySelector(".clear-cart");
-const cartDOM = document.querySelector(".cart");
-const cartOverlay = document.querySelector(".cart-overlay");
-const cartItems = document.querySelector(".cart-itemas");
-const cartTotal = document.querySelector(".cart-total");
-const cartContent = document.querySelector(".cart-content");
-const productsDOM = document.querySelector(".products-center");
+const cartBtn      = document.querySelector(".cart-btn"       );
+const closeCartBtn = document.querySelector(".close-cart"     );
+const clearCartBtn = document.querySelector(".clear-cart"     );
+const cartDOM      = document.querySelector(".cart"           );
+const cartOverlay  = document.querySelector(".cart-overlay"   );
+const cartItems    = document.querySelector(".cart-itemas"    );
+const cartTotal    = document.querySelector(".cart-total"     );
+const cartContent  = document.querySelector(".cart-content"   );
+const productsDOM  = document.querySelector(".products-center");
 
 // cart
 let cart = [];
 
-// gettin the products
+// getting the products
 class Products {
   async getProducts() {
     try {
-      let result = await fetch("products.json");
-      let data = await result.json();
-      return data;
+      const result = await fetch("products.json");
+      const data = await result.json();
+      let products = data.items;
+      products = products.map(item => {
+        const { title, price } = item.fields;
+        const { id } = item.sys;
+        const image = item.fields.image.fields.file.url;
+        return { title, price, id, image }
+      })
+      return products;
     } catch (error) {
       console.log(error);
     }
